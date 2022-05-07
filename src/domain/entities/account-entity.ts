12889fb139e802ff4a555a2ee4c402d5daf8eff3
Saulo Almeida <saulohenrique786@gmail.com>
2022-05-07 17:26:24 -0300

    refactor: add baseentity type to entities

--- src/domain/entities/account-entity.ts
+++ src/domain/entities/account-entity.ts
@@ -1,11 +1,27 @@
-class Account {
+import { BaseEntity } from "./common/base-entity";
+
+class Account implements BaseEntity {
+  constructor(id: string, 
+              nickname: string | undefined, 
+              login: string, 
+              password: string, 
+              note: string | undefined) {
+    this.id = id;
+    this.nickname = nickname;
+    this.login = login;
+    this.password = password;
+    this.note = note;
+    this.created_at = new Date();
+  }
+
   id!: string;
-  created_at!: Date;
-  updated_at: Date | undefined;
   nickname: string | undefined;
   login!: string;
   password!: string;
   note: string | undefined;
+
+  created_at: Date;
+  updated_at?: Date | undefined;
 }
 
 export default Account;
--- src/domain/entities/password-entity.ts
+++ src/domain/entities/password-entity.ts
@@ -1,11 +1,31 @@
 import Account from "./account-entity";
+import { BaseEntity } from "./common/base-entity";
 
-class Password {
+class Password implements BaseEntity {
+  constructor(id: string,
+    icon_url: string,
+    nickname: string,
+    website: string) {
+      this.id = id;
+      this.icon_url = icon_url;
+      this.name = nickname;
+      this.website = website;
+
+      this.accounts = new Array<Account>();
+      this.created_at = new Date();
+  }
   id!: string;
   icon_url!: string;
   name!: string;
   website!: string;
   accounts!: Account[];
+
+  created_at: Date;
+  updated_at?: Date | undefined;
+
+  addAccount(account: Account): void {
+    this.accounts.push(account);
+  }
 }
 
 export default Password;
